60bd0c237ea8cd2ce165a5abc97bd35b889f15da
meghamarwarapy <megha.marwarpay@gmail.com>
2024-11-23 12:54:51 +0530

    setsize

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -24,7 +24,6 @@ import assetimg from "../../assets/images/Login.gif";
 import logo from "../../assets/images/logologin.png";
 import bg from "../../assets/images/bgimgmarwar.jpg";
 
-
 const API_ENDPOINT = `${domainBase}apiUser/v1/userRoute/login`;
 
 const Login = () => {
@@ -44,37 +43,24 @@ const Login = () => {
         password,
       });
 
-      // Extract tokens from response data
       const { accessToken, refreshToken } = response.data.data;
-      
 
       if (accessToken && refreshToken) {
-        // Save tokens or user data if needed
         localStorage.setItem("accessToken", accessToken);
         localStorage.setItem("refreshToken", refreshToken);
 
-        if (rememberMe) {
-          // Save the expiration time for 24 hours
-          const expirationTime = new Date().getTime() + 24 * 60 * 60 * 1000;
-          localStorage.setItem("expirationTime", expirationTime);
-        } else {
-          // Save a shorter expiration time if "Remember me" is not checked
-          const expirationTime = new Date().getTime() + 60 * 60 * 1000;
-          localStorage.setItem("expirationTime", expirationTime);
-        }
+        const expirationTime = new Date().getTime() + (rememberMe ? 24 : 1) * 60 * 60 * 1000;
+        localStorage.setItem("expirationTime", expirationTime);
 
-        // Show success message
         setSnackbarMessage("Login successful!");
         setOpenSnackbar(true);
 
-        // Redirect to homepage/dashboard after a short delay
         setTimeout(() => {
           navigate("/");
         }, 2000);
       } else {
-        throw new Error("Access token or refresh token is missing in response.");
+        throw new Error("Access token or refresh token is missing.");
       }
-
     } catch (err) {
       console.error("Login error:", err);
       setSnackbarMessage("Login failed. Please try again.");
@@ -82,239 +68,174 @@ const Login = () => {
     }
   };
 
-  const handleCloseSnackbar = () => {
-    setOpenSnackbar(false);
-  };
+  const handleCloseSnackbar = () => setOpenSnackbar(false);
 
   return (
     <div style={{
       backgroundImage: `url(${bg})`,
       backgroundSize: "cover",
       backgroundPosition: "center",
-      backgroundRepeat: "no-repeat"
+      backgroundRepeat: "no-repeat",
+      height: "100vh", // Ensure the background covers the entire viewport height
     }}>
-     
-    
-    <Box
-      sx={{
-        padding:"92px 20px",
+      <Box sx={{
+        padding: "92px 20px",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-      }}
-    >
- <Container
-  // component="main"
-  sx={{
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  // Adjust the height as needed
-  }}
->
-  <Box
-    sx={{
-      display: "flex",
-      width: "100%",
-      justifyContent: "space-between",
-      alignItems: "center",
-    }}
-  >
-    {/* Left side image */}
-    <Box
-  sx={{
-    flex: 1, // Takes up 50% of the width
-    display: { xs: "none", sm: "flex" }, // Hide on extra-small screens (less than 500px)
-    justifyContent: "center",
-    alignItems: "center",
-  }}
->
-  <img
-    src={assetimg}
-    alt="Logo"
-    style={{
-      width: "100%", // Adjust the width of the image as needed
-      height: "auto",
-    }}
-  />
-</Box>
-
-
-    {/* Right side form */}
-    <Box
-      sx={{
-        flex: 1, // Takes up 50% of the width
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Paper
-        elevation={3} 
-        sx={{
-          padding: 4,
+      }}>
+        <Container sx={{
           display: "flex",
-          flexDirection: "column",
           alignItems: "center",
-          backgroundColor: "rgba(255, 255, 255, 0.5)",
-          borderRadius: 2,
-          width: "100%",
-          maxWidth: "400px", // Adjust the form width as needed
-        }}
-      >
-            <Box
-            sx={{
+          justifyContent: "center",
+        }}>
+          <Box sx={{
+            display: "flex",
+            flexDirection: { xs: "column", sm: "row" },
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+          }}>
+            {/* Left side image */}
+            <Box sx={{
+              flex: 1,
+              display: { xs: "none", sm: "flex" },
+              justifyContent: "center",
+              alignItems: "center",
+            }}>
+              <img src={assetimg} alt="Login" style={{
+                width: "100%",
+                height: "auto",
+              }} />
+            </Box>
+
+            {/* Right side form */}
+            <Box sx={{
+              flex: 1,
               display: "flex",
               justifyContent: "center",
-              width: "100%",
-              position: 'relative',
-              // right:'50%',
-            }}
-          >
-            <img
-              src={logo}
-              alt="Logo"
-              style={{
-                // width: 70,
-                // height: 70,
-                
-                position: 'absolute',
-                bottom: '-1px'
-              }}
-            />
+              alignItems: "center",
+              padding: { xs: 2, sm: 0 },
+            }}>
+              <Paper elevation={3} sx={{
+                padding: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                borderRadius: 2,
+                width: "100%",
+                maxWidth: "400px",
+              }}>
+                <Box sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  width: "100%",
+                  position: "relative",
+                }}>
+                  <img src={logo} alt="Logo" style={{
+                    position: 'absolute',
+                    bottom: '-1px',
+                  }} />
+                </Box>
+                <Typography component="h1" variant="h4" gutterBottom sx={{ fontWeight: 700, marginTop: "20px" }}>
+                  Log In
+                </Typography>
+                <Box component="form" onSubmit={handleLogin} sx={{ width: "100%" }}>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="userName"
+                    label="User Name"
+                    name="userName"
+                    autoComplete="userName"
+                    autoFocus
+                    value={userName}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={rememberMe}
+                        onChange={(e) => setRememberMe(e.target.checked)}
+                      />
+                    }
+                    label="Remember me"
+                    sx={{
+                      "& .MuiFormControlLabel-label": {
+                        color: "#1976d2",
+                        "&:hover": {
+                          color: "black",
+                        },
+                      },
+                    }}
+                  />
+                  <Button type="submit" fullWidth variant="contained" color="primary" sx={{ mt: 3, mb: 2 }}>
+                    Sign In
+                  </Button>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link href="#" variant="body2" sx={{
+                        color: "#1976d2",
+                        textDecoration: "none",
+                        "&:hover": { color: "black" },
+                      }}>
+                        Forgot password?
+                      </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link href="#" variant="body2" sx={{
+                        color: "#1976d2",
+                        textDecoration: "none",
+                        "&:hover": { color: "black" },
+                      }}>
+                        {"Don't have an account? Sign Up"}
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Paper>
+            </Box>
           </Box>
-        <Typography
-          component="h1"
-          variant="h4"
-          gutterBottom
-          style={{ fontWeight: 700, marginTop: "20px" }}
-        >
-          Log In
-        </Typography>
-        <Box component="form" onSubmit={handleLogin} sx={{ width: "100%" }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="userName"
-            label="User Name"
-            name="userName"
-            autoComplete="userName"
-            autoFocus
-            value={userName}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={rememberMe}
-                onChange={(e) => setRememberMe(e.target.checked)}
-              />
-            }
-            label="Remember me"
-            sx={{
-              "& .MuiFormControlLabel-label": {
-                color: "#1976d2",
-                "&:hover": {
-                  color: "black",
-                },
-              },
-            }}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link
-                href="#"
-                variant="body2"
-                sx={{
-                  color: "#1976d2",
-                  textDecoration: "none",
-                  "&:hover": {
-                    color: "black",
-                  },
-                }}
-              >
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link
-                href="#"
-                variant="body2"
-                sx={{
-                  color: "#1976d2",
-                  textDecoration: "none",
-                  "&:hover": {
-                    color: "black",
-                  },
-                }}
-              >
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Paper>
-    </Box>
-  </Box>
-</Container>
+        </Container>
 
+        {/* Snackbar for notifications */}
+        <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+          <Alert onClose={handleCloseSnackbar} severity={snackbarMessage.includes("failed") ? "error" : "success"} sx={{ width: '100%' }}>
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
+      </Box>
 
-      
-
-      {/* Snackbar for notifications */}
-      <Snackbar
-  open={openSnackbar}
-  autoHideDuration={6000}
-  onClose={handleCloseSnackbar}
-  anchorOrigin={{ vertical: 'top', horizontal: 'right' }} // Set position to top-right
->
-  <Alert 
-    onClose={handleCloseSnackbar} 
-    severity={snackbarMessage.includes("failed") ? "error" : "success"}
-    sx={{ width: '100%' }} // Ensure full-width for better appearance
-  >
-    {snackbarMessage}
-  </Alert>
-</Snackbar>
-    </Box>
-    <Box
-        sx={{
-          marginTop:'-80px',
-          display: "flex",
-          paddingBottom:'18px'
-          // gap: 1,
-        }}
-      >
-        <IconButton href="https://www.facebook.com/Marwarpay/"   target="_blank" sx={{ color: "black", fontSize: 50 }}>
+      {/* Social media icons */}
+      <Box sx={{
+        marginTop: '-80px',
+        display: "flex",
+        justifyContent: "center",
+        gap: 2,
+        paddingBottom: '18px',
+      }}>
+        <IconButton href="https://www.facebook.com/Marwarpay/" target="_blank" sx={{ color: "black", fontSize: 50 }}>
           <FacebookIcon sx={{ fontSize: "inherit" }} />
         </IconButton>
-        <IconButton href="https://www.instagram.com/marwarpayofficial/"  target="_blank"  sx={{ color: "black", fontSize: 50 }}>
+        <IconButton href="https://www.instagram.com/marwarpayofficial/" target="_blank" sx={{ color: "black", fontSize: 50 }}>
           <InstagramIcon sx={{ fontSize: "inherit" }} />
         </IconButton>
-        <IconButton href="https://www.twitter.com"  target="_blank"  sx={{ color: "black", fontSize: 50 }}>
+        <IconButton href="https://www.twitter.com" target="_blank" sx={{ color: "black", fontSize: 50 }}>
           <TwitterIcon sx={{ fontSize: "inherit" }} />
         </IconButton>
       </Box>

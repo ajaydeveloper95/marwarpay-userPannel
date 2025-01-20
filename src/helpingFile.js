b122df0e4c878c3bd91cacba32f348926b6e04df
raviMarwarpay <ravi.marwarpay@gmail.com>
2025-01-20 10:37:41 +0530

    initial commit

--- src/helpingFile.js
+++ src/helpingFile.js
@@ -1,4 +1,4 @@
-export const domainBase = "https://api.zanithpay.com/";
-// export const domainBase = "http://localhost:5000/";
+// export const domainBase = "https://api.zanithpay.com/";
+export const domainBase = "http://localhost:5000/";
 export const accessConstent = "accessToken";
 export const accessRefresh = "refreshToken";
